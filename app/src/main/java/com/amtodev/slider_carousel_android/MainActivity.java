bdfccf310a1de9e315af5ccdedf84cc0f6866b90
manasesortez <manasesortez1234@hotmail.com>
2022-02-05 10:25:31 -0600

    carousel - slider

--- app/src/main/java/com/amtodev/slider_carousel_android/MainActivity.java
+++ app/src/main/java/com/amtodev/slider_carousel_android/MainActivity.java
@@ -2,7 +2,12 @@ package com.amtodev.slider_carousel_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
@@ -11,6 +16,7 @@ import com.smarteist.autoimageslider.SliderView;
 public class MainActivity extends AppCompatActivity {
 
     SliderView sliderView;
+    Button button1;
     int[] images = {R.drawable.image1,
             R.drawable.images2,
             R.drawable.images3,
@@ -33,5 +39,20 @@ public class MainActivity extends AppCompatActivity {
         sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
         sliderView.startAutoCycle();
 
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent =new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.imdb.com/title/tt4154796/"));
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+
+
 }
