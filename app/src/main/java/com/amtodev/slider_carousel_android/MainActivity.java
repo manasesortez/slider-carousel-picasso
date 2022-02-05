5630f0cca06c71ee195c374dcfae39598f63350a
manasesortez <manasesortez1234@hotmail.com>
2022-02-05 10:06:27 -0600

    carousel - slider

--- app/src/main/java/com/amtodev/slider_carousel_android/MainActivity.java
+++ app/src/main/java/com/amtodev/slider_carousel_android/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
 public class MainActivity extends AppCompatActivity {
+
+    SliderView sliderView;
+    int[] images = {R.drawable.image1,
+            R.drawable.images2,
+            R.drawable.images3,
+            R.drawable.images4,
+            R.drawable.images5,
+            R.drawable.images6};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+
     }
 }
